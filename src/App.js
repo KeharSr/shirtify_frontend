0e4387caa03d0b98e0592a316a2d577f71194c52
Kehar Kumar Ojha <keharojha56@gmail.com>
2024-12-26 20:46:48 +0545

    partical work completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 import Register from "./pages/register/Register";
 import Login from "./pages/login/Login";
+import AdminRoutes from "./pages/protected_routes/AdminRoutes";
+import AdminPage from "./pages/admin/AdminPage";
+import EditAdminProfile from "./pages/admin/EditProfile";
 
 function App() {
   return (
@@ -12,6 +15,12 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
 
+        {/* Admin Protected Routes */}
+        <Route element={<AdminRoutes />}>
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/adminprofile" element={<EditAdminProfile />} />
+          {/* <Route path='/add-product' element={<AddProduct />} /> */}
+        </Route>
 
       </Routes>
     </Router>
--- src/apis/Apis.js
+++ src/apis/Apis.js
@@ -36,4 +36,98 @@ export const googleLoginApi = (data) => Api.post("/api/user/google", data);
 export const getUserByGoogleEmail = (data) =>
 	Api.post(`/api/user/getGoogleUser`, data);
 
+// get current user api
+export const getCurrentUserApi = () => Api.get('/api/user/current',config)
+
+// edit user profile api
+export const editUserProfileApi = (data) => Api.put('/api/user/update',data,config)
+
+//Upload Profile Picture Api
+export const uploadProfilePictureApi = (data) => Api.post('/api/user/profile_picture',data)
+
+
+// Create Product Api
+export const createProductApi = (data) => Api.post('/api/product/create', data)
+
+// Get All Products Api
+export const getAllProductsApi = () => Api.get('/api/product/get_all_products',config)
+
+// Update Product Api
+export const updateProduct = (id,data) => Api.put(`/api/product/update_product/${id}`,data,config)
+
+//delete product api
+export const deleteProduct = (id) => Api.delete(`/api/product/delete_product/${id}`, config)
+
+// Get Single Product Api
+export const getSingleProductApi = (id) => Api.get(`/api/product/get_single_product/${id}`,config)
+
+// Get Products By Category including pagination
+export const getProductsByCategoryApi = (category, page, limit=2 ) =>  Api.get(`/api/product/get_products_by_category?category=${category}&page=${page}&limit=${limit}`, config)
+
+
+//=========================== Cart Apis ===========================
+
+// Add to Cart Api
+export const addToCartApi = (data) => Api.post('/api/cart/add_to_cart', data,config)
+
+// Get Cart Api
+export const getCartApi = () => Api.get('/api/cart/get_cart',config)
+
+// delete cart item api
+export const deleteCartItemApi = (id) => Api.delete(`/api/cart/remove_cart_item/${id}`,config)
+
+// update cart status api
+export const updateStatusApi = () => Api.put(`/api/cart/update_status`,'',config)
+
+// update cart item
+export const updateCartItemApi=(data)=> Api.put(`/api/cart/update_cart`,data,config)
+
+
+//=========================== Order Apis ===========================
+//place order api
+export const placeOrderApi = (data) => Api.post('/api/order/place_order',data,config2)
+
+// get single order api
+export const getSingleOrderApi = (id) => Api.get(`/api/order/get_single_order/${id}`,config)
+
+// get all orders api
+export const getAllOrdersApi = () => Api.get('/api/order/get_all_orders',config)
+
+// order status update api
+export const updateOrderStatusApi = (id,data) => Api.post(`/api/order/update_order_status/${id}`,data,config2)
+
+// get orders by user api
+export const getOrdersByUserApi = () => Api.get('/api/order/get_orders_by_user',config)
+
+
+//=========================== Review Apis ===========================
+
+// add review api
+export const addReviewApi = (data) => Api.post('/api/review/post_reviews', data,config)
+
+// get reviews api
+export const getReviewsApi = (ProductId) => Api.get(`/api/review/get_reviews/${ProductId}`,config)
+
+// get reviews by product and user api
+export const getReviewsByProductAndUserApi = (ProductId) => Api.get(`/api/review/get_reviews_by_user_and_product/${ProductId}`,config) 
+
+// get average rating api
+export const getAverageRatingApi = (ProductId) => Api.get(`/api/review/get_average_rating/${ProductId}`,config)
+
+//update review api
+export const updateReviewApi = (productId,data) => Api.put(`/api/review/update_reviews/${productId}`,data,config)
+
+
+//=========================== Favourites Apis ===========================
+
+// add to favourites api
+export const addFavouriteApi = (data) => Api.post('/api/favourite/add_favourite', data,config)
+
+// get favourites api
+export const getFavouritesApi = () => Api.get('/api/favourite/get_favourite',config)
+
+// delete favourite api
+export const deleteFavouriteApi = (id) => Api.delete(`/api/favourite/remove_favourite/${id}`,config)
+
+
 export default Api;
